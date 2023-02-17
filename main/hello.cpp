0538bed474d9e05c396879ec14cafc3c04521e6d
varsharoopesh <118705717+varsharoopesh@users.noreply.github.com>
2023-02-17 10:59:46 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout << "i am varsha"
+  cout << "I,Varsha have successfully built and run\n";
+  cout << "PES1UG20CS185, Sec D"
   return 0;
 }
